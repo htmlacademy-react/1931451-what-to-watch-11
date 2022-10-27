7266fd1e7ab17e093b06103aa883b430ec89d48b
UnionGhost <takeofme@gmail.com>
2022-10-27 22:05:52 +0300

    module2-task1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type MainPageProps = {
+  name: string;
+  genre: string;
+  releaseDate: number;
+}
+
+function App(props: MainPageProps): JSX.Element {
+  return <MainPage name={props.name} genre={props.genre} releaseDate={props.releaseDate} />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const DataFilm = {
+  Name: 'The Grand Budapest Hotel',
+  Genre: 'Drama',
+  ReleaseDate: 2014,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      name={DataFilm.Name}
+      genre={DataFilm.Genre}
+      releaseDate={DataFilm.ReleaseDate}
+    />
   </React.StrictMode>,
 );
